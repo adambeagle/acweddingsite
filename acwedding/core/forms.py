70814b399af039212b2b725f1a5037d04de65e02
Adam Beagle <abeagle88@gmail.com>
2014-07-28 09:25:44 -0700

    core/forms.py cleanup; Template change

--- acwedding/core/forms.py
+++ acwedding/core/forms.py
@@ -6,19 +6,22 @@ from django import forms
 from django.conf import settings
 from django.core.mail import send_mail
 
-
 class ContactForm(forms.Form):
-    error_css_class = 'form-error'
-    required_css_class = 'form-required'
+    """
+    Basic "contact us" form. On is_valid, sends email using form data to owner.
+    """
     name = forms.CharField(max_length=64, label='Your name')
     email_address = forms.EmailField(label='Your email address')
     cc_sender = forms.BooleanField(required=False, label='Send copy of message to email address above')
     message = forms.CharField(widget=forms.Textarea)
     
+    error_css_class = 'form-error'
+    
     def send_email(self):
         """
         Sends email to owner based on form data.
         Uses django.core.mail's send_email, which uses EMAIL_HOST_* (etc.) settings.
+        Assumes existence of DOMAIN and CONTACT_EMAIL environment variables.
         Expects self.cleaned_data to be populated, so must be called after is_valid().
         """
         cleaned = self.cleaned_data
@@ -40,8 +43,3 @@ class ContactForm(forms.Form):
             sbj = 'Your message to {0}'.format(domain)
             recipients = [cleaned['email_address']]
             send_mail(sbj, msg, fromaddr, recipients)
-        
-        
-        
-    
-    
