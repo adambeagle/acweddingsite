9af9dd2c82450b8b134581891957b285da983b9a
Adam Beagle <abeagle88@gmail.com>
2014-09-03 20:49:16 -0700

    Remake thumbnails to all be square. This appears to be the best solution to easily have a large grid of bootstrap .thumbnails.
    util/make_thumbnails also updated to make square thumbnails

--- util/make_thumbnails.py
+++ util/make_thumbnails.py
@@ -4,58 +4,15 @@ requirements. See docs/installation.rst.
 
 PURPOSE:
 
-This module was written to automate creating thumbnails for static images
-in a Django project.
+This module was written to automate the creation of thumbnails.
 
-Please note that much of this file wasn't written with general usage in mind;
-my particular repository and Django project structure are assumed.
-The make_thunbnails function could be easily used as-is in any project,
-however.
-
-USAGE:
-
-1. Change PROJECT_NAME to match the name of your Django project.
-   Note this is only necessary for usage as described in 3b.
-
-2. If desired, change MAX_SIZE and FORMATS below, just below the imports. 
-   See the Pillow docs for supported formats.
-   
-There are two ways to use the module:
-
-3a. If called with no arguments, a thumbnail will be made of any images 
-    located in the same folder as this file whose extension matches one
-    found in FORMATS. The thumbnail will be created in this folder as 
-    <file>.<original extension>.thumbnail
-    
-3b. If called with two arguments, srcdir and destdir, thumbnails will be made
-    of images in '<repo root>/acwedding/static/images/<srcdir>' and placed in 
-    '<repo root>/<PROJECT_NAME>/static/images/<destdir>.' Thumbnails will have the
-    file format <original filename>.<original extension>.thumbnail
-
-USAGE NOTES:
-
-* Thumbnails are saved using the png format, regardless of the
-  original image's format. The original extension of the image remains in
-  its thumbnail filename to make accessing a thumbnail within a template
-  easy (merely append '.thumbnail' to the file's original complete name).
-  
-* srcdir is not traversed recursively.
-   
-EXAMPLE USAGE:
-
-To create thumbnails of every image found in 
-<repo root>/<PROJECT_NAME>/static/gallery and place them in 
-<repo root>/<PROJECT_NAME>/static/thumbnails,
-run 'python make_thumbnails.py gallery thumbnails' at the command line.
 """
 from os import listdir, path
 from re import match, IGNORECASE
 from sys import argv
 
-from PIL import Image
-from unipath import Path
+from PIL import Image, ImageOps
 
-PROJECT_NAME = 'acwedding'
 MAX_SIZE = (100, 100)
 FORMATS = ('png', 'jpg', 'jpeg') # Case-insensitve
 
@@ -71,7 +28,7 @@ def make_thumbnails(srcdir, destdir):
     for f in listdir(srcdir):
         if match(pattern, f, IGNORECASE):
             image = Image.open(path.join(srcdir, f))
-            image.thumbnail(MAX_SIZE, Image.ANTIALIAS)
+            image = ImageOps.fit(image, MAX_SIZE, Image.ANTIALIAS)
             
             destfile = path.join(destdir, f + '.thumbnail')
             image.save(destfile, 'png')
@@ -88,20 +45,10 @@ if __name__ == '__main__':
             raise ArgumentsError('Usage: python make_thumbnails.py' +
                 ' <srcdir> <destdir>')
         
-        repoRoot = Path(path.abspath(__file__)).ancestor(2)
-        srcdir = str(repoRoot.child(PROJECT_NAME, 'static', 'images', srcdir))
-        destdir = str(repoRoot.child(PROJECT_NAME, 'static', 'images', destdir))
-        
         if not path.isdir(srcdir):
             raise DirectoryDoesNotExistError(srcdir)
             
         if not path.isdir(destdir):
             raise DirectoryDoesNotExistError(destdir)
-    
-    # If no arguments passed, thumbnails will be made of any images in the 
-    # same folder as this file.
-    else:
-        filePath = Path(path.abspath(__file__)).parent
-        srcdir = destdir = str(filePath)
         
-    make_thumbnails(srcdir, destdir)
+    make_thumbnails(path.abspath(srcdir), path.abspath(destdir))
